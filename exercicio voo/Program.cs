61603be848681dc6176d6b241f1f2be477f82aaa
Gabriely Maritns <gabrielybudim@gmail.com>
2023-06-13 13:51:39 -0400

    voo

---
+++ exercicio voo/Program.cs
@@ -0,0 +1,38 @@
+﻿Voo voo = new Voo(DateTime.Now, 428);
+while (true)
+{
+    Console.WriteLine("Menu");
+    Console.WriteLine("1- Ver o próxima poltrona livre");
+    Console.WriteLine("2- Verificar as poltronas");
+    Console.WriteLine("3- Ocupar as poltronas");
+    Console.WriteLine("4- Vagas de poltronas disponíveis");
+
+    Console.WriteLine("Escolha um opção do menu:");
+    int opcaoEscolhida = Convert.ToInt32(Console.ReadLine());
+
+    if (opcaoEscolhida == 1)
+    {
+        Console.WriteLine("A próxima poltrona livre é: " + voo.ProximoLivre());
+    }
+    if (opcaoEscolhida == 2)
+    {
+        Console.WriteLine("Informe a poltrona para verificar:");
+        int poltrona = Convert.ToInt32(Console.ReadLine());
+        voo.Verificar(poltrona);
+    }
+    if (opcaoEscolhida == 3)
+    {
+        Console.WriteLine("Informe a poltrona para ocupar:");
+        int poltrona = Convert.ToInt32(Console.ReadLine());
+        voo.Ocupar(poltrona);
+    }
+    if (opcaoEscolhida == 4)
+    {
+        Console.WriteLine("As vagas disponíveis é: " + voo.Vagas());
+    }
+
+}
+
+
+
+
---
+++ exercicio voo/voo.cs
@@ -0,0 +1,90 @@
+﻿
+public class Voo
+{
+    private DateTime dataHora;
+    private int numeroVoo;
+    public bool[] passageiros = new bool[100];
+
+    public Voo(DateTime dataHora, int numeroVoo)
+    {
+        this.dataHora = dataHora;
+        this.numeroVoo = numeroVoo;
+    }
+    public DateTime GetDataHora()
+    {
+        return this.dataHora;
+    }
+
+    public int GetNumeroVoo()
+    {
+        return this.numeroVoo; 
+    }
+
+    public void SetDataHora(DateTime dataHora) 
+    { 
+        this.dataHora = dataHora;
+    }
+
+    public void SetVoo(int numeroVoo) 
+    {
+        this.numeroVoo = numeroVoo;
+    }
+
+    public int ProximoLivre()
+    {
+        for (int i = 0; i < passageiros.Length; i++)
+        {
+            if (passageiros[i] == false)
+            {
+                return i;
+                break;
+            }
+        }
+        Console.WriteLine("Infelizmente não tem poltronas livres.");
+        return -1;
+    }
+
+    public bool Ocupar(int nCadeira)
+    {
+        if (this.passageiros[nCadeira] == false)
+        {
+            this.passageiros[nCadeira] = true;
+
+            Console.WriteLine("Essa poltrona está dispónivel");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Poltrona já está ocupada!");
+            return false;
+        }
+    }
+
+    public bool Verificar(int nCadeira)
+    {
+        if (this.passageiros[nCadeira] == false) 
+        {
+            Console.WriteLine("Poltrona disponível");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Poltrona ocupada!");
+            return false; 
+        }   
+    }
+
+    public int Vagas()
+    {
+        int cont = 0;
+        for(int i = 0;i < passageiros.Length;i++) 
+        { 
+            if (this.passageiros[i] == false)
+            {
+                cont +=1;
+            }
+        }
+        return cont;
+    }
+   
+}
